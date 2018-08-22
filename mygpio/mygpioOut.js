3403c5a8421f7a9a3bb8297b178a146a7952b537
jiin995 <gabrieleprevitera@gmail.com>
2018-08-22 16:27:48 +0000

    aggiunto controllo su pin null

--- mygpio/mygpioOut.js
+++ mygpio/mygpioOut.js
@@ -6,17 +6,16 @@ module.exports = function(RED) {
 		
         node.on('input', function(msg) {
 			var spawn = require('child_process').spawn;
-
 			var gpioCommand = __dirname+'/nrgpio';
 
 			var pin=msg.payload[0];
 				//console.log("Pin: "+pin);
 			var value=msg.payload[1];
 				//console.log("Value: "+value);
-				
-			node.child = spawn(gpioCommand, ["out",pin,value]);
-			node.child.stdin.write("close "+pin);
-
+			if(pin!==null){
+				node.child = spawn(gpioCommand, ["out",pin,value]);
+				node.child.stdin.write("close "+pin);
+			}
         });
     }
     RED.nodes.registerType("mygpio-out",MyGpioOutNode);
